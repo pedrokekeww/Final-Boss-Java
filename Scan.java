57b65715f706b942293f435ab446d44c2230c43f
pedrokekww <pedroveb119@gmail.com>
2025-02-12 17:53:42 -0300

    Create Scan.java

---
+++ Scan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;  // Import the Scanner class
+class Scan {
+  public static void main(String[] args) {
+    int NumAparelhos
+      
+    Scanner aparelhos = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+    int NumAparelhos = aparelhos.nextLine();  // Read user input
+
+    for(int i=0; i <= NumAparelhos; i++)
+      {
+          
+      }
