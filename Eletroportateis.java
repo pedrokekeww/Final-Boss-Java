5fdee15eab650a8bcf1fb6885836bdcfad2ff9da
Alyne001 <alyne.souza62@aluno.ifce.edu.br>
2025-02-14 15:27:37 -0300

    Update Eletroportateis.java

--- Eletroportateis.java
+++ Eletroportateis.java
@@ -1,4 +1,29 @@
 package Projeto;
 public class Eletroportateis extends Eletronico {
+ public double tempo; 	// Em horas
+	public double energia;
+	Eletroportateis(String nome, String modelo, double potencia, double tempo){
+		super(nome, modelo, potencia);
+		this.tempo = tempo;
+	}
+      
+	@Override
+	public void calcEnergia() {
+		energia = (potencia/1000) * tempo;
+	}
+        @Override
+	public double calcEnergia7() {
+		return energia * 7;
+	}
+        @Override
+	public double calcEnergia30() {
+		return energia * 30;
+	}
+       @Override
+	public double calcEnergia365() {
+		return energia * 365;
+	}
+}
+
 
 }
