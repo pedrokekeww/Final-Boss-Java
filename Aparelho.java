6dd6df33029125e74e76694c8366443da56d56d2
L0RR4 <arlensonlorran@gmail.com>
2025-02-25 22:09:02 -0300

    Update Aparelho.java

--- Aparelho.java
+++ Aparelho.java
@@ -1,32 +1,32 @@
-package br.com.projeto.calc3000;
+package br.com.poo.projetoFinal.calculadoraDeEnergia;
 
 public abstract class Aparelho {
-    private double potencia; // em Watts
+	private double potencia; // em Watts
     private double tempo; // em horas
-    protected static final double PRECO_KWH = 0.80; // Preço por kWh (exemplo)
+    protected static final double PRECO_KWH = 0.636; // Preço por kWh em SP(2024)
 
     public Aparelho() {}
-
+    
     public Aparelho(double potencia, double tempo) {
         this.potencia = potencia;
         this.tempo = tempo;
     }
-
+    
     public double getPotencia() {
         return potencia;
     }
-
+    
     public void setPotencia(double potencia) {
         this.potencia = potencia;
     }
-
+    
     public double getTempo() {
         return tempo;
     }
-
+    
     public void setTempo(double tempo) {
         this.tempo = tempo;
     }
-
+    
     public abstract double calcularCusto(); // Método abstrato
 }
