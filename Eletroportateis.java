f17933f3b728eff1c3aa66529ff61b083fe88bfd
L0RR4 <arlensonlorran@gmail.com>
2025-02-25 22:21:27 -0300

    Update Eletroportateis.java

--- Eletroportateis.java
+++ Eletroportateis.java
@@ -1,20 +1,20 @@
-package br.com.projeto.calc3000;
+package br.com.poo.projetoFinal.calculadoraDeEnergia;
 
 //Eletroportátil herda de Aparelho e implementa TipoAparelho
 public class Eletroportatil extends Aparelho implements TipoAparelho {
- public Eletroportatil() {}
+	public Eletroportatil() {}
 
- public Eletroportatil(double potencia, double tempo) {
-     super(potencia, tempo);
- }
+	public Eletroportatil(double potencia, double tempo) {
+		super(potencia, tempo);
+		}
 
- @Override
- public double calcularCusto() {
-     return (getPotencia() * getTempo() / 1000) * PRECO_KWH * 1.1; // Taxa extra de 10% para eletroportáteis
- }
+	@Override
+	public double calcularCusto() { // Sobescreve o método da SuperClasse
+		return (getPotencia() * getTempo() / 1000) * PRECO_KWH * 1.1; // Taxa extra de 10% para eletroportáteis
+		}
 
- @Override
- public String getTipo() {
-     return "Eletroportátil";
- }
+	@Override
+	public String getTipo() { // Sobescreve o método da Interface
+	    return "Eletroportátil";
+	   }
 }
