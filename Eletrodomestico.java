68b09c38c96e1824be482bc56b08dfcdcdba90a1
L0RR4 <arlensonlorran@gmail.com>
2025-02-25 22:15:17 -0300

    Update Eletrodomestico.java

--- Eletrodomestico.java
+++ Eletrodomestico.java
@@ -1,20 +1,20 @@
-package br.com.projeto.calc3000;
+package br.com.poo.projetoFinal.calculadoraDeEnergia;
 
 //Eletrodoméstico herda de Aparelho e implementa TipoAparelho
 public class Eletrodomestico extends Aparelho implements TipoAparelho {
- public Eletrodomestico() {}
+	public Eletrodomestico() {}
 
- public Eletrodomestico(double potencia, double tempo) {
-     super(potencia, tempo);
- }
+	public Eletrodomestico(double potencia, double tempo) {
+		super(potencia, tempo);
+		}
 
- @Override
- public double calcularCusto() {
-     return (getPotencia() * getTempo() / 1000) * PRECO_KWH;
- }
+	@Override 
+	public double calcularCusto() { // Sobescreve o método da SuperClasse
+		return (getPotencia() * getTempo() / 1000) * PRECO_KWH;
+		}
 
- @Override
- public String getTipo() {
-     return "Eletrodoméstico";
- }
+	@Override
+	public String getTipo() { // Sobescreve o método da interface
+		return "Eletrodoméstico";
+		}
 }
